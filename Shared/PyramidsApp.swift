d5a782d714fd0c04e8a59d79d3f9aabf5502e32a
Vladimir Fibe <motiw@icloud.com>
2022-01-12 23:57:51 +0600

    Initial Commit

---
+++ Shared/PyramidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyramidsApp.swift
+//  Shared
+//
+//  Created by Vladimir Fibe on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct PyramidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
